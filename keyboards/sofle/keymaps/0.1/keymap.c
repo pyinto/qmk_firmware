5129047bacb391c51d3c4295de0a0f6b4de7d3fa
vladimirk <pyinto@gmail.com>
2022-02-18 14:59:30 +0800

    refactor: volume fix

--- keyboards/sofle/keymaps/0.1/keymap.c
+++ keyboards/sofle/keymaps/0.1/keymap.c
@@ -386,9 +386,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         if (clockwise) {
