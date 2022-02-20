ac167b29bc1623596264358ec97ad8bc66e31215
vladimirk <pyinto@gmail.com>
2022-02-20 16:38:16 +0800

    cleaning up default layout

--- keyboards/sofle/keymaps/homeraw_mod/keymap.c
+++ keyboards/sofle/keymaps/homeraw_mod/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC,
   KC_TAB,  HOME_A, HOME_S, HOME_D, HOME_F, KC_G,                     KC_H,  HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_MUTE,   XXXXXXX, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
-                 XXXXXXX,XXXXXXX,XXXXXXX, KC_LGUI, KC_ENT,  KC_SPC, KC_RAISE, XXXXXXX,XXXXXXX,XXXXXXX
+               XXXXXXX,XXXXXXX,XXXXXXX, KC_LGUI, KC_ENT,     KC_SPC, KC_RAISE, XXXXXXX,XXXXXXX,XXXXXXX
 ),
 
 /* LOWER
