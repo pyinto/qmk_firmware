9b606b85e32f10709d2ccd00f8777b4b6503ba62
vladimirk <pyinto@gmail.com>
2022-02-20 16:54:54 +0800

    cleaning up default layout

--- keyboards/sofle/keymaps/homeraw_mod/keymap.c
+++ keyboards/sofle/keymaps/homeraw_mod/keymap.c
@@ -30,6 +30,12 @@ enum custom_keycodes {
 #define HOME_SCLN RCTL_T(KC_SCLN)
 //MT(MOD_RSFT, KC_J), MT(MOD_RGUI, KC_K), MT(MOD_RALT, KC_L), MT(MOD_RCTL, KC_SCLN),
 
+// JK to esc combo
+//const uint16_t PROGMEM test_combo2[] = {MT(MOD_RSFT, KC_J), MT(MOD_RGUI, KC_K), COMBO_END};
+//combo_t key_combos[COMBO_COUNT] = {
+//    COMBO(test_combo2, KC_ESC),
+//};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -49,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,  KC_7,    KC_8,    KC_9,    KC_0,      KC_LBRC,
-  KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC,
-  KC_TAB,  HOME_A, HOME_S, HOME_D, HOME_F, KC_G,                     KC_H,  HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_MUTE,   XXXXXXX, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
-               XXXXXXX,XXXXXXX,XXXXXXX, KC_LGUI, KC_ENT,     KC_SPC, KC_RAISE, XXXXXXX,XXXXXXX,XXXXXXX
+  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC,
+  KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+  KC_TAB , HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,                      KC_H   , HOME_J , HOME_K , HOME_L , HOME_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE,    XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                    XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, KC_ENT ,    KC_SPC , KC_RAISE, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* LOWER
@@ -112,29 +118,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-  [_ADJUST] = LAYOUT(
-  XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RESET  , XXXXXXX,KC_QWERTY,XXXXXXX,CG_TOGG,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX,CG_TOGG, XXXXXXX,    XXXXXXX,  XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
-                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+[_ADJUST] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
   )
 };
 
-/* [_EMPTY_LAYOUT] = LAYOUT(
- * _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
- * _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
- * _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
- * _______, _______, _______, _______, _______, _______,  _______,       _______,  _______, _______, _______, _______, _______, _______,
- *              _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______
- * ),
- */
+//[_EMPTY_LAYOUT] = LAYOUT(
+//  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+//  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+//  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+//  _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+//                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
+//),
 
-// JK to esc combo
-//const uint16_t PROGMEM test_combo2[] = {MT(MOD_RSFT, KC_J), MT(MOD_RGUI, KC_K), COMBO_END};
-//combo_t key_combos[COMBO_COUNT] = {
-//    COMBO(test_combo2, KC_ESC),
-//};
+
+
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
