0738819af86457fff1959e5fa03e09d11f7536f7
vladimirk <pyinto@gmail.com>
2022-02-21 22:19:11 +0800

    adding tabs switch on knob

--- keyboards/sofle/keymaps/homeraw_mod/keymap.c
+++ keyboards/sofle/keymaps/homeraw_mod/keymap.c
@@ -205,10 +205,10 @@ static int8_t ticks = 0;
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
+        if (clockwise) { ++ticks; } else { --ticks; }
+        if (abs(ticks) >= 2) {
+            tap_code16(clockwise ? KC_VOLD: KC_VOLU);
+            ticks = 0;
         }
     } else if (index == 1) {
         if (clockwise) { ++ticks; } else { --ticks; }
