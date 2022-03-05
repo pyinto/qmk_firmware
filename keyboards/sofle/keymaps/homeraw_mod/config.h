40ae242851a33f74e30b74b015081c25f3de41ad
vladimirk <pyinto@gmail.com>
2022-03-05 23:48:31 +0800

    updates

--- keyboards/sofle/keymaps/homeraw_mod/config.h
+++ keyboards/sofle/keymaps/homeraw_mod/config.h
@@ -2,8 +2,7 @@
 #undef TAPPING_TERM
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 320
-
+#define TAPPING_TERM 200
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
