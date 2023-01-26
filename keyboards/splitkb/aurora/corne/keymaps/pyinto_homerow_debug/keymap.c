e189f550f8a12a9ec08986e3f823c900b4e9e68a
volodymyr_k <pyinto@gmail.com>
2023-01-26 16:03:32 +0000

    adjusting sofle to corne(fix)

--- keyboards/splitkb/aurora/corne/keymaps/pyinto_homerow_debug/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/pyinto_homerow_debug/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `---------------------------''------'          '------''---------------------------'
  */
 
-[_QWERTY] = LAYOUT(
+[_QWERTY] = LAYOUT_split_3x6_3(
   KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
   KC_TAB , HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,                      KC_H   , HOME_J , HOME_K , HOME_L , HOME_SCLN, KC_QUOT,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,                         KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |//////|/       /        \       \|      |      |      |      |
  *            `---------------------------''-------'         '------''---------------------------'
  */
-[_LOWER] = LAYOUT(
+[_LOWER] = LAYOUT_split_3x6_3(
   _______, _______, KC_PAST, KC_PLUS, KC_PEQL, _______,                      _______, KC_7   , KC_8   , KC_9   , _______, KC_DEL  ,
   _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                      _______, KC_4   , KC_5   , KC_6   , KC_0   , _______,
   _______, _______, KC_CIRC, KC_PERC, KC_MINS, _______,                      _______, KC_1   , KC_2   , KC_3   , KC_PDOT, _______,
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /        \       \|//////|      |      |      |
  *            `---------------------------''------'          '------''---------------------------'
  */
-[_RAISE] = LAYOUT(
+[_RAISE] = LAYOUT_split_3x6_3(
   _______, KC_DLR , KC_AT  , KC_QUES, KC_EXLM, _______,                      KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, _______, _______,
   _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TILD,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_UNDS, _______,
   _______, KC_PIPE, KC_AMPR, KC_GRV , KC_HASH, _______,                      KC_LABK, KC_LPRN, KC_RPRN, KC_RABK, _______, _______,
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /        \       \|      |      |      |      |
  *            `---------------------------''------'          '------''---------------------------'
  */
-[_ADJUST] = LAYOUT(
+[_ADJUST] = LAYOUT_split_3x6_3(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
