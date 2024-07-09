7ca41cc112c4ae8f3594b035393e69d3780be49e
volodymyr_k <pyinto@gmail.com>
2024-07-09 09:50:32 +0100

    chore: changing tapping term

--- keyboards/splitkb/aurora/corne/keymaps/pyinto_homerow_debug/config.h
+++ keyboards/splitkb/aurora/corne/keymaps/pyinto_homerow_debug/config.h
@@ -28,12 +28,15 @@
     #define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+
+
 #undef TAPPING_TERM
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 195
+
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
