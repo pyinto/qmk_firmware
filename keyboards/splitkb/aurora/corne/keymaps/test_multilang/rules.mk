f530d888648382d8adcd273b39f770140ccd61b2
volodymyr_k <pyinto@gmail.com>
2024-07-29 16:44:14 +0100

    chore: adding test layout

---
+++ keyboards/splitkb/aurora/corne/keymaps/test_multilang/rules.mk
@@ -0,0 +1,23 @@
+# # # Hardware specific settings: # # #
+CONVERT_TO=liatris
+PIN_COMPATIBLE=promicro
+
+ENCODER_ENABLE = no
+OLED_ENABLE = no
+BACKLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = yes
+
+
+# # # Software specific settings: # # #
+COMBO_ENABLE = yes
+
+CAPS_WORD_ENABLE = yes  # https://docs.qmk.fm/features/caps_word
+
+NO_SUSPEND_POWER_DOWN = yes  # Sleep issue fix
+
+EXTRAKEY_ENABLE = yes  # Audio control and System control
+
+# CONSOLE_ENABLE = yes  # For debug
+
+#UNICODEMAP_ENABLE = yes
